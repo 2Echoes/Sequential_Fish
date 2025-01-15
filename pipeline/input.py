275d16822b909117e986134f1fa38d85855ebbdb
Flo <floric.slimani@live.fr>
2025-01-15 15:55:53 +0100

    _folder intergrity moved in tools

--- pipeline/input.py
+++ pipeline/input.py
@@ -5,7 +5,7 @@ This script aims at reading the input folder and preparing data folders and loca
 from Sequential_Fish.pipeline_parameters import RUN_PATH, FOLDER_KEYS, MAP_FILENAME, cycle_regex, CYCLE_KEY, GENES_NAMES_KEY, WASHOUT_KEY_WORD, HAS_BEAD_CHANNEL
 from Sequential_Fish.pipeline.tools.utils import open_image, auto_map_channels
 
-import pipeline.tools._folder_integrity as prepro
+import Sequential_Fish.pipeline.tools._folder_integrity as prepro
 import pandas as pd
 import os
 import re
