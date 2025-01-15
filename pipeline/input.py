3d96f4e52e91b6e4d5d584fdace4bc4d4b06409f
Flo <floric.slimani@live.fr>
2025-01-15 16:37:57 +0100

    quickfix to be remvoed when file is corrected

--- pipeline/input.py
+++ pipeline/input.py
@@ -35,11 +35,11 @@ COLUMNS = [
     ]
 Acquisition = pd.DataFrame(columns=COLUMNS)
 cycle_map = pd.read_excel(RUN_PATH + '/' + MAP_FILENAME)
-cycle_number = len(cycle_map)
-print(cycle_map)
 ## TO REMOVE
 cycle_map = cycle_map.drop(21)
+print(cycle_map)
 color_number = len(GENES_NAMES_KEY)
+cycle_number = len(cycle_map)
 
 
 file_index = 0
