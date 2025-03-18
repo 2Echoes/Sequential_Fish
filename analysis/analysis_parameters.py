24408cdb08a9417c29fc9ad479193e29bda9e66a
Flo <floric.slimani@live.fr>
2025-03-18 14:10:05 +0100

    new parameter : RNA_filter

---
+++ analysis/analysis_parameters.py
@@ -0,0 +1,19 @@
+
+RUN_PATH = "/mnt/ssd/SSD_floricslimani/Fish_seq/Davide/2024-10-09 - SeqFISH_Run10 - HeLa-Puro_POLR2"
+FILTER_RNA = ['POLR2A_20']
+
+
+#Distributions
+distribution_measures = [
+    'rna_number', 
+    'cluster_number', 
+    'proportion_rna_in_foci', 
+    'nb_rna_in_nuc', 
+    'index_mean_distance_nuc', 
+    'index_mean_distance_cell'
+    ]
+
+# Density analysis
+min_diversity = 3
+min_spots_number = 3
+cluster_radius = 400 #nm
