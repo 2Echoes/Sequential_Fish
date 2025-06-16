b62fd23c2b37354f77ad81190bfb0511c8771e2b
2Echoes <floric.slimani@live.fr>
2025-06-16 10:23:59 +0200

    init

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import os
 
-__version__ = 0.1
+__version__ = 0.2
 __script_dir__ = os.path.dirname(os.path.abspath(__file__))
 __run_cache_path__ = os.path.join(__script_dir__, "run_saves", "Run_cache.feather")
--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -2,8 +2,6 @@
 Entry point to launch Sequential Fish analysis pipeline.
 """
 
-__version__ = 0.1
-
 def run(*args) :
     from .runner import main
     main()
